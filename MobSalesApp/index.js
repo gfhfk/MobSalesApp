3702c1291c8bbecbcfdef3f13257290664097d33
o.metelskiy@hotmail.com <o.metelskiy@hotmail.com>
2013-11-19 17:13:05 +0200

    product list

---
+++ MobSalesApp/index.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+window.MyApp = {};
+
+$(function () {
+    FastClick.attach(document.body);
+
+    var devices = DevExpress.devices;
+    var defaultLayout = "slideout";
+    if (devices.current().platform === "desktop") {
+        defaultLayout = "desktop";
+    }
+    MyApp.app = new DevExpress.framework.html.HtmlApplication({
+        namespace: MyApp,
+        
+        defaultLayout: defaultLayout,
+        navigation: [
+          {
+            title: "Home",
+            action: "#home",
+            icon: "home"
+          },
+          {
+            title: "About",
+            action: "#about",
+            icon: "info"
+          }
+        ]
+    });
+    MyApp.app.router.register(":view", {view: "home"});
+    MyApp.app.navigate();   
+});
