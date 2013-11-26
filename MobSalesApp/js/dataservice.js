6d8d91f6eec5ab26dffb52fad9c3c8930b3d2b03
gfhfk <o.metelskiy@hotmail.com>
2013-11-27 01:37:07 +0200

    Add Breeze

---
+++ MobSalesApp/js/dataservice.js
@@ -0,0 +1,15 @@
+﻿!function ($, DX, app, undefined) {
+    var DATA_VERSION_KEY = "mobilesales-version",
+    DATA_KEY = "mobilesales-data";
+
+
+
+
+    $.extend(wo, {
+
+        initUserData: initUserData,
+        //clearUserData: clearUserData,
+
+    });
+
+}(jQuery, DevExpress, MobileSales);
